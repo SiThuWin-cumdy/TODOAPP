e844bed135a283430525dd4909eaa0dabe626212
Si Thu Win <winst@corpit.com.sg>
2023-09-27 20:39:30 +0630

    changes for edit

--- app/edittopic/[id]/page.jsx
+++ app/edittopic/[id]/page.jsx
@@ -2,8 +2,8 @@ import EditTopicComponent from "@/components/EditTopicComponent";
 
 const getTopicById = async (id) => {
   try {
-    // const apiURL = "https://todoapp-rosy-nine.vercel.app";
-    const res = await fetch(`api/topics/${id}`);
+    const apiURL = process.env.NEXT_PUBLIC_API_ROUT;
+    const res = await fetch(`${apiURL}/api/topics/${id}`);
     if (!res.ok) {
       throw new Error("Faild to get data");
     }
