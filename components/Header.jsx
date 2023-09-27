b893b61971abaff4510f94e5771526d42f52b169
Si Thu Win <winst@corpit.com.sg>
2023-09-27 20:27:59 +0630

    chagnes

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <nav>
       <div className="flex justify-between items-center px-5 py-10 bg-slate-800">
         <Link className="text-white font-bold" href={"/"}>
-          MyApp
+          TodoApp
         </Link>
         <Link className="bg-white p-2" href={"/addtopic"}>
           AddTopic
